01887be42dd503ed54fa2896ee238f56a51f0894
Sergei Morozov <morozov@tut.by>
2026-06-10 15:34:24 -0600

    Serve I, R, IFF1, IFF2, and IM as GDB registers
    Expose the Z80 interrupt and refresh state over the GDB stub's register
    protocol so clients read and write it as first-class registers instead
    of scraping debugger output.

--- debugger/arch.h
+++ debugger/arch.h
@@ -4,6 +4,12 @@
 #include <stdint.h>
 
 #define SZ 2
+/* The core CPU feature mirrors the standard GDB z80 register set and order,
+   ending in the combined ir register. Interrupt state (iff1/iff2/im) and the
+   profiling clock live in separate fuse-emulator features; their names embed
+   "z80" because clients (z88dk-gdb) only harvest registers from features whose
+   name matches z80. Register order here MUST match the index order in
+   gdbserver.c's get_register_value/set_register_value. */
 #define FEATURE_STR "l<target version=\"1.0\">"\
     "<feature name=\"org.gnu.gdb.z80.cpu\">"\
     "<reg name=\"af\" bitsize=\"16\" type=\"int\"/>"\
@@ -18,14 +24,18 @@
     "<reg name=\"bc'\" bitsize=\"16\" type=\"int\"/>"\
     "<reg name=\"de'\" bitsize=\"16\" type=\"int\"/>"\
     "<reg name=\"hl'\" bitsize=\"16\" type=\"int\"/>"\
+    "<reg name=\"ir\" bitsize=\"16\" type=\"int\"/>"\
+    "</feature>"\
+    "<feature name=\"org.fuse-emulator.z80.interrupt\">"\
+    "<reg name=\"iff1\" bitsize=\"16\" type=\"int\"/>"\
+    "<reg name=\"iff2\" bitsize=\"16\" type=\"int\"/>"\
+    "<reg name=\"im\" bitsize=\"16\" type=\"int\"/>"\
+    "</feature>"\
+    "<feature name=\"org.fuse-emulator.z80.timing\">"\
     "<reg name=\"clockl\" bitsize=\"16\" type=\"int\"/>"\
     "<reg name=\"clockh\" bitsize=\"16\" type=\"int\"/>"\
     "</feature>"\
     "<architecture>z80</architecture>"\
     "</target>"
-
-#define EXTRA_NUM 25
-#define EXTRA_REG 16
-#define EXTRA_SIZE 4
 
 #endif /* ARCH_H */
